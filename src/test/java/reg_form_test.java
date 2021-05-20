9a6cf18ba1940a1ffb2797bfdcc425fb2c2f6ea2
platff <ds.platff@gmail.com>
2021-05-20 20:42:49 +0300

    src files created

--- src/test/java/reg_form_test.java
+++ src/test/java/reg_form_test.java
@@ -1,17 +1,72 @@
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+/*import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;*/
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.*;
 
-public class SearchTests {
+public class reg_form_test {
+
+    //static Logger log = LoggerFactory.getLogger(reg_form_test.class);
+
+    @BeforeAll
+    static void setUpBrowser() {
+        System.out.println("Starting tests; setting up browser....");
+
+        Configuration.browser = "chrome";
+        Configuration.startMaximized = true;
+
+    }
+
+    @BeforeEach
+    void openTestPage(){
+
+        String PageURL = "https://demoqa.com/automation-practice-form";
+
+        System.out.println("Test page opening...");
+
+        open(PageURL);
+
+    }
+
+    @AfterEach
+    void closeBrowser() {
+        System.out.println("Test ended. Closing browser...");
+        closeWebDriver();
+    }
 
     @Test
     void selenideSearchTest() {
-        open("https://www.google.com/");
 
-        $(byName("q")).setValue("selenide").pressEnter();
+        String firstNameText = "John";
+        String lastNameText  = "Dow";
+        String firstNameFieldID  = "firstName";
+        String lastNameFieldID   = "lastName";
+        String userEmail = "nomail@nomail.org";
+        String phoneNumber = "1234567890";
 
-        $("#search").shouldHave(text("selenide.org"));
+        //log.info("Starting form test....");
+        System.out.println("Starting form test....");
+
+        $(byId(firstNameFieldID)).setValue(firstNameText);
+        $(byId(lastNameFieldID)).setValue(lastNameText);
+
+        $("#userEmail").setValue(userEmail);
+        $("#gender-radio-3").doubleClick();
+        $("#userNumber").setValue(phoneNumber);
+
+        // DoB selection
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__day--014").click();
+
+        // Press button
+        $(byId("submit")).pressEnter();
+
+        System.out.println("Ending form test....");
     }
 }
